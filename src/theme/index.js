aa84c2f9d8a22828b56e12d8219bd2e8be4d0965
Luciano Santos <luciusjss@gmail.com>
2018-12-26 13:25:35 +0000

    fix background color

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,10 +1,13 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import { pink, deepPurple } from '@material-ui/core/colors';
+import { pink, deepPurple, grey } from '@material-ui/core/colors';
 
 export default createMuiTheme({
     palette: {
         primary: deepPurple,
-        secondary: pink
+        secondary: pink,
+        background: {
+            default: grey[200]
+        }
     },
     typography: {
         useNextVariants: true
