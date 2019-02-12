bc724ba6cdb7a89fc4862f489ccc3f6deb5cf00f
Luciano Santos <lucianosantos@MacBook-Pro-de-Luciano.local>
2019-02-12 15:01:27 +0100

    service worker added

--- src/index.js
+++ src/index.js
@@ -7,3 +7,8 @@ import App from './App';
 const rootElement = document.getElementById('root');
 
 render(<App />, rootElement);
+
+if ('serviceWorker' in navigator) {
+    // sw.js can literally be empty, but must exist
+    navigator.serviceWorker.register('/serviceWorker.js');
+}
