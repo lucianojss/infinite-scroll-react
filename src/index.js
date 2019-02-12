7e606a03e0e43d7dac2fb8ed3add72e8b13c9be2
Luciano Santos <lucianosantos@MacBook-Pro-de-Luciano.local>
2019-02-12 17:06:29 +0100

    added service worker

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
-
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 const rootElement = document.getElementById('root');
 
 render(<App />, rootElement);
 
-if ('serviceWorker' in navigator) {
-    // sw.js can literally be empty, but must exist
-    navigator.serviceWorker.register('/serviceWorker.js');
-}
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.register();
