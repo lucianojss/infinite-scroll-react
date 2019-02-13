f4c6204ffec535ab0b168b92bc3f0ca305d95969
Luciano Santos <luciusjss@gmail.com>
2019-02-13 17:40:46 +0100

    fix fonts

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,5 +1,6 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import { pink, deepPurple, grey } from '@material-ui/core/colors';
+import 'typeface-roboto';
 
 export default createMuiTheme({
     palette: {
